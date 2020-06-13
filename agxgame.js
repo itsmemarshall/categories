e83f3e309f1dcb61c1d3765b4b0167f6a0b74a47
Alice Zhao <alicezhao@Alices-MBP.home>
2020-06-13 16:44:53 -0400

    picture

--- agxgame.js
+++ agxgame.js
@@ -2,8 +2,8 @@ var io;
 var gameSocket;
 
 // Game constants.
-var rounds = 3;
-var categoriesPerRound = 3;
+var rounds = 1;
+var categoriesPerRound = 1;
 var roundTime = 5;
 var categories = new Array(rounds)
 for (i = 0; i < rounds; i++) {
